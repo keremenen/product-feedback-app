d22c4f68d23dc19b1348f646cb2ed5e8e2333877
Przemysław Kitowski <kitowski.przemek@gmail.com>
2024-10-18 16:00:50 +0200

    Add main page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,8 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { ChevronUpIcon } from "lucide-react";
+import commentIcon from "../icons/icon-comments.svg";
 
 const jost = localFont({
   src: "../fonts/Jost-VariableFont_wght.ttf",
@@ -123,44 +125,93 @@ export default function RootLayout({
               </ul>
             </div>
           </header>
-          <section className="flex-grow">
-            <div className="flex flex-row items-center gap-[27px] rounded-md bg-primary-dark-blue px-4 py-[0.875rem] pl-6">
-              {/* SUGGESTIONS */}
-              <div className="flex items-center gap-4">
-                <Image src={suggestionIcon} alt="suggestion icon" />
+          <section className="flex flex-grow flex-col gap-6">
+            <section>
+              <div className="flex flex-row items-center gap-[27px] rounded-md bg-primary-dark-blue px-4 py-[0.875rem] pl-6">
+                {/* SUGGESTIONS */}
+                <div className="flex items-center gap-4">
+                  <Image src={suggestionIcon} alt="suggestion icon" />
 
-                <h2 className="text-heading-3 text-primary-white">
-                  6 Suggestions
-                </h2>
+                  <h2 className="text-heading-3 text-primary-white">
+                    6 Suggestions
+                  </h2>
+                </div>
+
+                {/* SELECT FORM */}
+                <div className="relative text-primary-white">
+                  <Select>
+                    <SelectTrigger className="border-none bg-transparent focus:ring-transparent">
+                      <span className="font-normal">Sort by :</span>
+                      <SelectValue placeholder="Most Upvotes" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem value="light">Most Upvotes</SelectItem>
+                      <SelectSeparator />
+                      <SelectItem value="dark">Least Upvotes</SelectItem>
+                      <SelectSeparator />
+                      <SelectItem value="system">Most Comments</SelectItem>
+                      <SelectSeparator />
+                      <SelectItem value="valuer">Least Comments</SelectItem>
+                    </SelectContent>
+                  </Select>
+                </div>
+
+                {/* BUTTON */}
+                <div className="ml-auto">
+                  <Button variant={"default"}>
+                    <Image src={plusIcon} alt="plus icon" />
+                    Add Feedback
+                  </Button>
+                </div>
               </div>
-
-              {/* SELECT FORM */}
-              <div className="relative text-primary-white">
-                <Select open>
-                  <SelectTrigger className="border-none bg-transparent focus:ring-transparent">
-                    <span className="font-normal">Sort by :</span>
-                    <SelectValue placeholder="Most Upvotes" />
-                  </SelectTrigger>
-                  <SelectContent>
-                    <SelectItem value="light">Most Upvotes</SelectItem>
-                    <SelectSeparator />
-                    <SelectItem value="dark">Least Upvotes</SelectItem>
-                    <SelectSeparator />
-                    <SelectItem value="system">Most Comments</SelectItem>
-                    <SelectSeparator />
-                    <SelectItem value="valuer">Least Comments</SelectItem>
-                  </SelectContent>
-                </Select>
-              </div>
-
-              {/* BUTTON */}
-              <div className="ml-auto">
-                <Button variant={"default"}>
-                  <Image src={plusIcon} alt="plus icon" />
-                  Add Feedback
+            </section>
+            <section className="flex flex-col gap-5">
+              <section className="flex flex-row gap-10 rounded-md bg-primary-white px-8 py-7">
+                <Button variant={"badge"} size={"icon-badge"}>
+                  <ChevronUpIcon />
+                  <span className="font-bold text-secondary-dark-blue">
+                    112
+                  </span>
                 </Button>
-              </div>
-            </div>
+                <div>
+                  <h3 className="mb-1 text-heading-3">
+                    Add tags for solutions
+                  </h3>
+                  <p className="mb-3 text-body-1 text-secondary-dark-grey">
+                    Easier to search for solutions based on a specific stack.
+                  </p>
+                  <Button variant={"badge"} size={"badge"}>
+                    Enhancement
+                  </Button>
+                </div>
+                <div className="ml-auto flex w-11 items-center gap-3">
+                  <Image src={commentIcon} alt="comment icon" />
+                  <span className="text-body-1 font-bold">2</span>
+                </div>
+              </section>
+              <section className="flex flex-row gap-10 rounded-md bg-primary-white px-8 py-7">
+                <Button variant={"badge"} size={"icon-badge"}>
+                  <ChevronUpIcon />
+                  <span className="font-bold text-secondary-dark-blue">99</span>
+                </Button>
+                <div>
+                  <h3 className="mb-1 text-heading-3">
+                    Add a dark theme option
+                  </h3>
+                  <p className="mb-3 text-body-1 text-secondary-dark-grey">
+                    It would help people with light sensitivities and who prefer
+                    dark mode.
+                  </p>
+                  <Button variant={"badge"} size={"badge"}>
+                    Feature
+                  </Button>
+                </div>
+                <div className="ml-auto flex w-11 items-center gap-3">
+                  <Image src={commentIcon} alt="comment icon" />
+                  <span className="text-body-1 font-bold">4</span>
+                </div>
+              </section>
+            </section>
           </section>
           {children}
         </main>
