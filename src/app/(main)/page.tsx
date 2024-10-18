abb42dbc4eaf4687a9303baf5cd69ff44896d2ad
Przemysław Kitowski <kitowski.przemek@gmail.com>
2024-10-18 16:31:27 +0200

    Move Feedbacklist into separate component (hardcoded data)

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,9 +1,4 @@
-import { Button } from "@/components/ui/button";
-
-import { ChevronUpIcon } from "lucide-react";
-import Image from "next/image";
-import commentIcon from "../../icons/icon-comments.svg";
-
+import FeedbackList from "@/components/feedback-list";
 import Logo from "@/components/logo";
 import Navigation from "@/components/navigation";
 import Roadmap from "@/components/roadmap";
@@ -14,54 +9,12 @@ export default function MainPage() {
     <main className="mx-auto mt-24 flex max-w-[1110px] flex-row gap-x-[1.875rem]">
       <header className="flex w-[255px] flex-col gap-6">
         <Logo />
-
         <Navigation />
-
         <Roadmap />
       </header>
       <section className="flex flex-grow flex-col gap-6">
         <SummaryBar />
-        <section className="flex flex-col gap-5">
-          <section className="flex flex-row gap-10 rounded-md bg-primary-white px-8 py-7">
-            <Button variant={"badge"} size={"icon-badge"}>
-              <ChevronUpIcon />
-              <span className="font-bold text-secondary-dark-blue">112</span>
-            </Button>
-            <div>
-              <h3 className="mb-1 text-heading-3">Add tags for solutions</h3>
-              <p className="mb-3 text-body-1 text-secondary-dark-grey">
-                Easier to search for solutions based on a specific stack.
-              </p>
-              <Button variant={"badge"} size={"badge"}>
-                Enhancement
-              </Button>
-            </div>
-            <div className="ml-auto flex w-11 items-center gap-3">
-              <Image src={commentIcon} alt="comment icon" />
-              <span className="text-body-1 font-bold">2</span>
-            </div>
-          </section>
-          <section className="flex flex-row gap-10 rounded-md bg-primary-white px-8 py-7">
-            <Button variant={"badge"} size={"icon-badge"}>
-              <ChevronUpIcon />
-              <span className="font-bold text-secondary-dark-blue">99</span>
-            </Button>
-            <div>
-              <h3 className="mb-1 text-heading-3">Add a dark theme option</h3>
-              <p className="mb-3 text-body-1 text-secondary-dark-grey">
-                It would help people with light sensitivities and who prefer
-                dark mode.
-              </p>
-              <Button variant={"badge"} size={"badge"}>
-                Feature
-              </Button>
-            </div>
-            <div className="ml-auto flex w-11 items-center gap-3">
-              <Image src={commentIcon} alt="comment icon" />
-              <span className="text-body-1 font-bold">4</span>
-            </div>
-          </section>
-        </section>
+        <FeedbackList />
       </section>
     </main>
   );
---
+++ src/components/feedback-list.tsx
@@ -0,0 +1,119 @@
+import { Button } from "@/components/ui/button";
+import { ChevronUpIcon } from "lucide-react";
+import commentIcon from "../icons/icon-comments.svg";
+import Image from "next/image";
+import { cn } from "@/lib/utils";
+
+const FeedbackListFromAPI = [
+  {
+    id: 1,
+    title: "Add tags for solutions",
+    category: "Enhancement",
+    description: "Easier to search for solutions based on a specific stack.",
+    comments: 2,
+    upvotes: 112,
+  },
+  {
+    id: 2,
+    title: "Add a dark theme option",
+    category: "Feature",
+    description:
+      "It would help people with light sensitivities and who prefer dark mode.",
+    comments: 4,
+    upvotes: 99,
+  },
+  {
+    id: 3,
+    title: "Q&A within the challenge hubs",
+    category: "Feature",
+    description: "Challenge-specific Q&A would make for easy reference.",
+    comments: 1,
+    upvotes: 65,
+  },
+  {
+    id: 4,
+    title: "Allow image/video upload to feedback",
+    category: "Enhancement",
+    description: "Images and screencasts can enhance comments on solutions.",
+    comments: 2,
+    upvotes: 51,
+  },
+  {
+    id: 5,
+    title: "Ability to follow others",
+    category: "Feature",
+    description: "Stay updated on comments and solutions other people post.",
+    comments: 3,
+    upvotes: 42,
+  },
+  {
+    id: 6,
+    title: "Preview images not loading",
+    category: "Bug",
+    description:
+      "Challenge preview images are missing when you apply a filter..",
+    comments: 0,
+    upvotes: 3,
+  },
+];
+
+export default function FeedbackList() {
+  return (
+    <section className="flex flex-col gap-5">
+      {FeedbackListFromAPI.map((feedback) => (
+        <FeedbackSingleItem
+          key={feedback.id}
+          title={feedback.title}
+          category={feedback.category}
+          description={feedback.description}
+          comments={feedback.comments}
+          upvotes={feedback.upvotes}
+        />
+      ))}
+    </section>
+  );
+}
+
+type FeedbackSingleItemProps = {
+  title: string;
+  category: string;
+  description: string;
+  comments: number;
+  upvotes: number;
+};
+
+function FeedbackSingleItem({
+  title,
+  category,
+  description,
+  comments,
+  upvotes,
+}: FeedbackSingleItemProps) {
+  return (
+    <section className="flex flex-row gap-10 rounded-md bg-primary-white px-8 py-7">
+      <Button variant={"badge"} size={"icon-badge"}>
+        <ChevronUpIcon />
+        <span className="font-bold text-secondary-dark-blue">{upvotes}</span>
+      </Button>
+      <div>
+        <h3 className="mb-1 text-heading-3">{title}</h3>
+        <p className="mb-3 text-body-1 text-secondary-dark-grey">
+          {description}
+        </p>
+        <Button variant={"badge"} size={"badge"}>
+          {category}
+        </Button>
+      </div>
+      <div className="ml-auto flex w-11 items-center gap-3">
+        <Image src={commentIcon} alt="comment icon" />
+        <span
+          className={cn("text-body-1 font-bold", {
+            "opacity-50": comments === 0,
+          })}
+        >
+          {comments}
+        </span>
+      </div>
+    </section>
+  );
+}
