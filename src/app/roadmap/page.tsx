df8254701095f1f461f128debb2f9cda5de044d8
Przemysław Kitowski <kitowski.przemek@gmail.com>
2024-10-22 18:54:38 +0200

    WIP main roadmap component

--- src/app/roadmap/page.tsx
+++ src/app/roadmap/page.tsx
@@ -6,7 +6,7 @@ export default function RoadmapPage() {
   return (
     <main className="mx-auto max-w-[1110px]">
       {/* TOPBAR SECTION */}
-      <section className="mt-[4.87rem] flex w-full items-center rounded-md bg-primary-dark-blue px-[2rem] py-[1.6875rem]">
+      <section className="mb-12 mt-[4.87rem] flex w-full items-center rounded-md bg-primary-dark-blue px-[2rem] py-[1.6875rem]">
         {/* NAVIGATION WITH TITLE */}
         <div className="flex flex-col items-start gap-y-1">
           <Button
@@ -29,6 +29,32 @@ export default function RoadmapPage() {
         </div>
       </section>
       {/* ROADMAP SECTION */}
+      <section className="flex flex-row justify-between gap-x-[1.875rem]">
+        <section className="max-w-[21.875rem] basis-1/3">
+          <div className="flex flex-col gap-y-1">
+            <h2 className="text-heading-3">Planned (2)</h2>
+            <p className="text-secondary-dark-grey">
+              Ideas prioritized for research
+            </p>
+          </div>
+        </section>
+
+        <section className="max-w-[21.875rem] basis-1/3">
+          <div className="flex flex-col gap-y-1">
+            <h2 className="text-heading-3">In-Progress (3)</h2>
+            <p className="text-secondary-dark-grey">
+              Currently being developed
+            </p>
+          </div>
+        </section>
+
+        <section className="max-w-[21.875rem] basis-1/3">
+          <div className="flex flex-col gap-y-1">
+            <h2 className="text-heading-3">Live (1)</h2>
+            <p className="text-secondary-dark-grey">Released features</p>
+          </div>
+        </section>
+      </section>
     </main>
   );
 }
