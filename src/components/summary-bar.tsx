f68878141d6c0d1017a8b693f0f7ed193a07a2d5
Przemysław Kitowski <kitowski.przemek@gmail.com>
2024-10-23 20:53:48 +0200

    Refactor summary bar component

--- src/components/summary-bar.tsx
+++ src/components/summary-bar.tsx
@@ -11,38 +11,36 @@ import { Button } from "./ui/button";
 
 export default function SummaryBar() {
   return (
-    <section>
-      <div className="flex flex-row items-center gap-[27px] rounded-md bg-primary-dark-blue px-4 py-[0.875rem] pl-6">
-        {/* SUGGESTIONS */}
-        <div className="flex items-center gap-4">
-          <SuggestionsIcon />
+    <section className="flex flex-row items-center gap-[27px] rounded-md bg-primary-dark-blue px-4 py-[0.875rem] pl-6">
+      {/* SUGGESTIONS */}
+      <div className="flex items-center gap-4">
+        <SuggestionsIcon />
 
-          <h2 className="text-heading-3 text-primary-white">6 Suggestions</h2>
-        </div>
+        <h2 className="text-heading-3 text-primary-white">6 Suggestions</h2>
+      </div>
 
-        {/* SELECT FORM */}
-        <div className="relative text-primary-white">
-          <Select>
-            <SelectTrigger className="bg-transparent pl-3 font-bold focus:ring-transparent">
-              <span className="font-normal">Sort by :</span>
-              <SelectValue placeholder="Most Upvotes" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectItem value="light">Most Upvotes</SelectItem>
-              <SelectSeparator />
-              <SelectItem value="dark">Least Upvotes</SelectItem>
-              <SelectSeparator />
-              <SelectItem value="system">Most Comments</SelectItem>
-              <SelectSeparator />
-              <SelectItem value="valuer">Least Comments</SelectItem>
-            </SelectContent>
-          </Select>
-        </div>
+      {/* SELECT FORM */}
+      <div className="relative text-primary-white">
+        <Select>
+          <SelectTrigger className="bg-transparent font-bold">
+            <span className="font-normal">Sort by: </span>
+            <SelectValue placeholder="Most Upvotes" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="light">Most Upvotes</SelectItem>
+            <SelectSeparator />
+            <SelectItem value="dark">Least Upvotes</SelectItem>
+            <SelectSeparator />
+            <SelectItem value="system">Most Comments</SelectItem>
+            <SelectSeparator />
+            <SelectItem value="valuer">Least Comments</SelectItem>
+          </SelectContent>
+        </Select>
+      </div>
 
-        {/* BUTTON */}
-        <div className="ml-auto">
-          <Button>+ Add Feedback</Button>
-        </div>
+      {/* BUTTON */}
+      <div className="ml-auto">
+        <Button>+ Add Feedback</Button>
       </div>
     </section>
   );
